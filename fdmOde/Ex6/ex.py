178a65d70bafbed45b78d12cf33fa8c0bfc48e54
Demengjkee <ncherepkin@live.com>
2016-05-25 12:08:56 +0300

    fixed bugs

--- fdmOde/Ex6/ex.py
+++ fdmOde/Ex6/ex.py
@@ -2,14 +2,15 @@ import sympy as sym
 import math
 import numpy as num
 
-t, omega, tau, p = sym.symbols('V t U omega tau i p')
+t, omega, tau, p = sym.symbols('t omega tau p')
 
 def countE(p):
-    return ((2 / p) ** 2) * (sym.sin(p / 2)) ** 2
+    return ((2 / p) ** 2) * (sym.sin(p / 2) ** 2)
 
 
-def tailor(E):
-    return E(math.pi) + sym.diff(((2 / p) ** 2) * (sym.sin(p / 2)) ** 2, p).subs(p, math.pi) * \
+def tailor():
+    return (((2 / p) ** 2) * (sym.sin(p / 2) ** 2)).subs(p, math.pi) + \
+	    sym.diff(((2 / p) ** 2) * (sym.sin(p / 2)) ** 2, p).subs(p, math.pi) * \
             (p + math.pi) + \
             sym.diff(((2 / p) ** 2) * (sym.sin(p / 2)) ** 2, p, 2).subs(p, math.pi) * \
             ((p + math.pi) ** 2) / math.factorial(2) + \
@@ -25,9 +26,9 @@ if __name__ == "__main__":
     s = num.linspace(0, math.pi, int(math.ceil(math.pi / tau)))
 
     with open("dotsE.dat", "w") as f:
-        for p in s:
-            f.write("" + str(p) + " " + str(countE(p)) + "\n")
+        for val in s:
+            f.write("" + str(val) + " " + str(countE(val)) + "\n")
 
     with open("tailor.dat", "w") as f:
-        f.write(str(tailor(E)) + "\n")
+        f.write(str(tailor()) + "\n")
 
